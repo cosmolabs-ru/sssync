41a974e6a005dbef9144d6324790f1a290f4b1be
cosmolabs <cosmolabs_avia@protonmail.com>
2021-07-28 19:51:04 +0300

    added better utc-2 handling

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     dt = datetime.utcnow().timetuple()
-    ss = ((dt.tm_hour-2) * 3600 + dt.tm_min * 60 + dt.tm_sec)*(1000/864)
+    hour = dt.tm_hour-2
+    if hour < 0:
+        hour = 24+hour
+    ss = (hour * 3600 + dt.tm_min * 60 + dt.tm_sec)*(1000/864)
     sstime = {"year": dt.tm_year-1, "month": floor(dt.tm_yday / 73), "date": dt.tm_yday % 73 - 1, "ss": ss}
     return dumps(sstime)
 
